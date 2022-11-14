b61f517fd15ecf055fcf311aae7f6492905cc6ba
Sebahauzer124 <sebahauzer124@gmail.com>
2022-11-13 23:39:32 -0300

    se sube

--- src/main/java/com/portfolio/sh/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/sh/Security/Controller/AuthController.java
@@ -31,8 +31,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/auth")
-@CrossOrigin(origins = "**")
+
+@CrossOrigin(origins = "https://argentinaprograma22.web.app/auth")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
