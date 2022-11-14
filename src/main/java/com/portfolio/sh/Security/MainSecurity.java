becdca292cf05f20008519347cdd7a47c737df4c
Sebahauzer124 <sebahauzer124@gmail.com>
2022-11-14 11:27:24 -0300

    ya no se como probar

--- src/main/java/com/portfolio/sh/Security/MainSecurity.java
+++ src/main/java/com/portfolio/sh/Security/MainSecurity.java
@@ -40,30 +40,19 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder();
     }
 
-   @Override
+     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-
-        List<String> list1 = Arrays.asList(new String[]{"Authorization", "Cache-Control", "Content-Type"});
-        List<String> list2 = Arrays.asList(new String[]{"https://argentinaprograma22.web.app"});
-        List<String> list3 = Arrays.asList(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"});
-        List<String> list4 = Arrays.asList(new String[]{"Authorization"});
-
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(list1);
-        corsConfiguration.setAllowedOrigins(list2);
-        corsConfiguration.setAllowedMethods(list3);
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(list4);
-
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("**").permitAll();
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors().configurationSource(request -> corsConfiguration);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);           
     }
+
 
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
